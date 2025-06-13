9720ac3f30fcaab423e6082bfca7f341938a1f4d
Reni-Rena <81166304+Reni-Rena@users.noreply.github.com>
2025-06-13 18:27:51 +0200

    sound + obj

--- Gestion Dechets/Assets/Script/Embalage.cs
+++ Gestion Dechets/Assets/Script/Embalage.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -5,17 +6,52 @@ public class Embalage : MonoBehaviour
 {
     public int score = 0;
     public TextMeshPro scoreText;
+    public GameObject cylindre;
+    public GameObject cylindreV;
+    public GameObject cylindreR;
+    
+    public float timer = 0f;
+    public bool actionFait = false;
+    
+    
+    private void Update()
+    {
+        scoreText.text = "" + score;
+        
+        if (!actionFait)
+        {
+            timer += Time.deltaTime;
 
+            if (timer >= 2f)
+            {
+                cylindreV.SetActive(false);
+                cylindreR.SetActive(false);
+                cylindre.SetActive(true);
+                
+                timer = 0f;
+                actionFait = true;
+            }
+        }
+    }
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(this.tag))
         {
             score++;
+            cylindre.SetActive(false);
+            cylindreV.SetActive(true);
+            actionFait = false;
+            Destroy(other.gameObject);
         }
         else if (!other.CompareTag("Untagged"))
         {
             score--;
+            cylindre.SetActive(false);
+            cylindreR.SetActive(true);
+            actionFait = false;
+            Destroy(other.gameObject);
         }
-        scoreText.text = "" + score;
     }
 }
